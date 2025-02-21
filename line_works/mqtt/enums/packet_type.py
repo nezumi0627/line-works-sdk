14d8334bd70e341b43f5fece7d3e4c01fc8e8144
nezumi0627 <nezumi.dev.0627@gmail.com>
2025-02-21 17:29:33 +0900

    Added description based on MQTT V3.1.1

--- line_works/mqtt/enums/packet_type.py
+++ line_works/mqtt/enums/packet_type.py
@@ -2,14 +2,22 @@ from enum import IntEnum
 
 
 class PacketType(IntEnum):
-    CONNECT = 1
-    CONNACK = 2
-    PUBLISH = 3
-    PUBACK = 4
-    UNKNWON = 6  # TODO: Rename when you know
-    UNKNWON_2 = 7  # TODO: Rename when you know
-    SUBSCRIBE = 8
-    SUBACK = 9
-    PINGREQ = 12
-    PINGRESP = 13
-    DISCONNECT = 14
+    CONNECT = 1       # 接続リクエスト
+    CONNACK = 2       # 接続応答
+
+    PUBLISH = 3       # メッセージ配信
+    PUBACK = 4        # QoS 1 配信確認
+    PUBREC = 5        # QoS 2 受信確認 (Part 1)
+    PUBREL = 6        # QoS 2 配信リリース (Part 2)
+    PUBCOMP = 7       # QoS 2 配信完了 (Part 3)
+
+    SUBSCRIBE = 8     # 購読リクエスト
+    SUBACK = 9        # 購読応答
+
+    UNSUBSCRIBE = 10  # 購読解除リクエスト
+    UNSUBACK = 11     # 購読解除応答
+
+    PINGREQ = 12      # 接続維持リクエスト
+    PINGRESP = 13     # 接続維持応答
+
+    DISCONNECT = 14   # 接続終了通知
