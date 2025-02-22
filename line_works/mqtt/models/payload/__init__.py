d73b0ab528eca61ddc40686b8f76547a1dc7c4d7
nezumi0627 <nezumi.dev.0627@gmail.com>
2025-02-22 18:53:31 +0900

    未検証であったため、initをロールバック

--- line_works/mqtt/models/payload/__init__.py
+++ line_works/mqtt/models/payload/__init__.py
@@ -2,43 +2,19 @@ from typing import Type
 
 from line_works.mqtt.enums.notification_type import NotificationType
 from line_works.mqtt.models.payload.badge import BadgePayload
-from line_works.mqtt.models.payload.message import MessagePayload
+from line_works.mqtt.models.payload.message import (
+    MessagePayload,
+)
 from line_works.mqtt.models.payload.service import ServicePayload
 
 PayloadTypes = MessagePayload | ServicePayload | BadgePayload
 
 NOTIFICATION_TYPE_MODEL_MAPPING: dict[int, Type[PayloadTypes]] = {
     NotificationType.NOTIFICATION_MESSAGE.value: MessagePayload,
-    NotificationType.NOTIFICATION_SERVICE.value: ServicePayload,
-    NotificationType.NOTIFICATION_BADGE.value: BadgePayload,
-    NotificationType.NOTIFICATION_UNKNOWN.value: MessagePayload,
-    NotificationType.NOTIFICATION_CONTACT.value: MessagePayload,
-    NotificationType.NOTIFICATION_LOCATION.value: MessagePayload,
-    NotificationType.NOTIFICATION_BOT.value: MessagePayload,
-    NotificationType.NOTIFICATION_OFFICIAL.value: MessagePayload,
-    NotificationType.NOTIFICATION_CHANNELNOTI.value: MessagePayload,
-    NotificationType.NOTIFICATION_RICH.value: MessagePayload,
-    NotificationType.NOTIFICATION_NEW_RICH.value: MessagePayload,
-    NotificationType.NOTIFICATION_IMAGE.value: MessagePayload,
-    NotificationType.NOTIFICATION_AUDIO.value: MessagePayload,
-    NotificationType.NOTIFICATION_RECORD.value: MessagePayload,
-    NotificationType.NOTIFICATION_VIDEO.value: MessagePayload,
-    NotificationType.NOTIFICATION_STICKER_OLD.value: MessagePayload,
-    NotificationType.NOTIFICATION_STICKER_V3.value: MessagePayload,
-    NotificationType.NOTIFICATION_FILE.value: MessagePayload,
-    NotificationType.NOTIFICATION_LOCATION_EMOJI.value: MessagePayload,
-    NotificationType.NOTIFICATION_CALL.value: MessagePayload,
-    NotificationType.NOTIFICATION_VCALL.value: MessagePayload,
-    NotificationType.NOTIFICATION_INVITE_DSSHARE.value: MessagePayload,
-    NotificationType.NOTIFICATION_STOP_DSSHARE.value: MessagePayload,
-    NotificationType.NOTIFICATION_CANCEL_CALL.value: MessagePayload,
-    NotificationType.NOTIFICATION_NEW_MAIL.value: MessagePayload,
-    NotificationType.NOTIFICATION_CAL_SCHD_REMIND.value: MessagePayload,
-    NotificationType.NOTIFICATION_CAL_TASK_REMIND.value: MessagePayload,
-    NotificationType.NOTIFICATION_CAL_APPO_INVITE.value: MessagePayload,
-    NotificationType.NOTIFICATION_CAL_APPO_UPDATE.value: MessagePayload,
-    NotificationType.NOTIFICATION_CAL_APPO_CANCEL.value: MessagePayload,
-    NotificationType.NOTIFICATION_API_EXEC_COMMAND.value: MessagePayload,
-    # 現在V3が使われているようなので、18をNOTIFICATION_STICKERとして定義
     NotificationType.NOTIFICATION_STICKER.value: MessagePayload,
+    NotificationType.NOTIFICATION_IMAGE.value: MessagePayload,
+    NotificationType.NOTIFICATION_LOCATION_EMOJI.value: MessagePayload,
+    NotificationType.NOTIFICATION_BADGE.value: BadgePayload,
+    NotificationType.NOTIFICATION_SERVICE.value: ServicePayload,
+    NotificationType.NOTIFICATION_FILE.value: MessagePayload,
 }
