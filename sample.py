845090ad0cb9d0f3c79017e82cc466de72b18ff1
nezumi0627 <nezumi.dev.0627@gmail.com>
2025-02-23 19:50:28 +0900

    add /flex command

--- sample.py
+++ sample.py
@@ -1,4 +1,6 @@
-from line_works.client import LineWorks
+import json
+
+from line_works.client import FlexContent, LineWorks
 from line_works.mqtt.enums.notification_type import NotificationType
 from line_works.mqtt.enums.packet_type import PacketType
 from line_works.mqtt.models.packet import MQTTPacket
@@ -22,6 +24,14 @@ def receive_publish_packet(w: LineWorks, p: MQTTPacket) -> None:
 
     elif payload.loc_args1 == "/msg":
         w.send_text_message(payload.channel_no, f"{payload!r}")
+
+    elif payload.loc_args1 == "/flex":
+        with open("src/sample_flex.json") as f:
+            j: dict = json.load(f)
+        w.send_flex_message(
+            payload.channel_no,
+            flex_content=FlexContent(alt_text="test", contents=j),
+        )
 
     if payload.notification_type == NotificationType.NOTIFICATION_STICKER:
         w.send_text_message(payload.channel_no, "スタンプ")
